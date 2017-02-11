342c90a457a4a232d3fc7f4532cc2b075893d4eb
Austin1244 <rixausti@msu.edu>
2017-02-11 13:54:26 -0500

    revisions

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // stored constants in objects for accessability
 var theme = { dark: 'night-theme', light: 'day-theme' }
 var storage = { visits: 'pageVisit', theme: 'pageTheme' }
-var page = { visits: 1, theme: theme.light }
+var page = { visits: 0, theme: theme.light }
 
 // get data from local storage
-page.visits = parseInt(window.localStorage.getItem(storage.visits))
-page.theme = window.localStorage.getItem(storage.theme)
+page.visits = parseInt(window.localStorage.getItem(storage.visits)) || page.visits
+page.theme = window.localStorage.getItem(storage.theme) || page.theme
 
-// condenced to one line because of simplicity
-page.theme === null ? page.theme = theme.light : page.theme
+page.visits++
+
 document.body.setAttribute('class', page.theme)
 
-// condenced to one line because of simplicity
-isNaN(page.visits) ? page.visits = 1 : page.visits++
 window.localStorage.setItem(storage.visits, page.visits)
-document.getElementById('visit-count').innerHTML = page.visits
+document.getElementById('visit-count').textContent = page.visits
 
 if (page.visits !== null) {
   window.localStorage.setItem(storage.visits, page.visits)
 }
 
 // Only appplies to one button, so I made it anonymous
-document.getElementsByName('themeButton')[0]
+document.getElementById('themeButton')
   .addEventListener('click', function () {
     var currentTheme = document.body.getAttribute('class')
     // switch the theme and set it
     document.body.setAttribute('class',
-      currentTheme = currentTheme === theme.dark ? theme.light : theme.dark
+      currentTheme = currentTheme === theme.dark
+        ? theme.light
+        : theme.dark
     )
     window.localStorage.setItem(storage.theme, currentTheme)
   })
